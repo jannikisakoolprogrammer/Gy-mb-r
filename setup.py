a6e02a33f7784b77f26a9ee4d82bb3a981da7bfe
jannikisakoolprogrammer <master4774@googlemail.com>
2021-09-12 14:07:53 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -11,9 +11,9 @@ if sys.platform == "win32":
     base = "Win32GUI"
 
 setup(
-    name = "Gyömbér",
+    name = "Gyoember",
     version = "0.1",
     description = "Learn Hungarian the hard way!",
     options = {"build_exe": build_exe_options},
-    executables = [Executable("gyömbér.py", base=base)]
+    executables = [Executable("Gyoember.py", base=base)]
 )
