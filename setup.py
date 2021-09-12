b08daac1713aab4b98a370241ee37a5735605f62
jannikisakoolprogrammer <master4774@googlemail.com>
2021-09-12 20:50:40 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,5 +1,6 @@
 import sys
 from cx_Freeze import setup, Executable
+import version as v_
 
 # Dependencies are automatically detected, but it might need fine tuning.
 # "packages": ["os"] is used as example only
@@ -13,7 +14,7 @@ if sys.platform == "win32":
 # Gyoember-0.1-win64.msi or find the file in Gyoember/dist/
 setup(
     name = "Gyoember",
-    version = "1.0.5",
+    version = v_.VERSION,
     description = "Learn Hungarian the hard way!",
     options = {"build_exe": build_exe_options},
     executables = [Executable("gyoember.py", base=base)]
