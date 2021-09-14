c6a660eb4eac632f7e862af20c38d886e4d914ab
jannikisakoolprogrammer <master4774@googlemail.com>
2021-09-15 01:15:22 +0200

    Increase to version 1.0.15.

--- version.py
+++ version.py
@@ -1,2 +1,2 @@
 import datetime
-VERSION = "%s-%s" % ("1.0.14", datetime.datetime.now().strftime("%Y_%m_%d_%H_%M_%S"))
+VERSION = "%s-%s" % ("1.0.15", datetime.datetime.now().strftime("%Y_%m_%d_%H_%M_%S"))
